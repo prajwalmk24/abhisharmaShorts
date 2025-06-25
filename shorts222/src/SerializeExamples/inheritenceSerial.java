ce4b7c340ccf4ee0a5383f2be12f40fe2d1d3113
prajwal mk <mkprajwal65@gmail.com>
2025-06-25 15:54:30 +0530

    inheritence serial

---
+++ shorts222/src/SerializeExamples/inheritenceSerial.java
@@ -0,0 +1,48 @@
+package SerializeExamples;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+
+/*Day 75*/
+public class inheritenceSerial {
+
+    // achieving serialization through inheritence
+    public static void main(String[] args) {
+
+
+        Student s1 = new Student(101, "Prajwal",'M');
+
+        //serialize
+        try {
+            FileOutputStream fileOut = new FileOutputStream("student.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+
+            out.writeObject(s1);
+
+            out.close();
+            fileOut.close();
+
+            System.out.println("Object has been serialized"+ s1);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //deserialize
+        try {
+            FileInputStream fileOut = new FileInputStream("student.ser");
+            ObjectInputStream out = new ObjectInputStream(fileOut);
+
+            Student stfObj = (Student) out.readObject();
+
+
+            System.out.println("Object has been de-serialized"+stfObj);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
