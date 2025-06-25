05bad5d7093926d047e9131911aae02f68ad5b32
prajwal mk <mkprajwal65@gmail.com>
2025-06-25 13:50:38 +0530

    deserialize example

--- shorts222/src/SerializeExamples/Student.java
+++ shorts222/src/SerializeExamples/Student.java
@@ -6,7 +6,9 @@ public class Student implements Serializable {
     int id;
     String name;
 
-    public Student(int i, String name) {
+    public Student(int id, String name) {
+        this.id=id;
+        this.name=name;
     }
 
     @Override
