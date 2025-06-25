40c69cb64721204750935e85b3d494136be8aec0
prajwal mk <mkprajwal65@gmail.com>
2025-06-25 13:51:01 +0530

    deserialize example

---
+++ shorts222/src/SerializeExamples/deserialize.java
@@ -0,0 +1,44 @@
+package SerializeExamples;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class deserialize {
+    public static void main(String[] args) {
+        Student s1 = new Student(101, "Prajwal");
+
+        //serialize
+        try {
+            FileOutputStream fileOut = new FileOutputStream("student.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+
+            out.writeObject(s1);
+//            out.close();
+//            fileOut.close();
+
+            System.out.println("Object has been serialized"+ s1);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //deserialize
+        try {
+            FileInputStream fileOut = new FileInputStream("student.ser");
+            ObjectInputStream out = new ObjectInputStream(fileOut);
+
+            Student stfObj = (Student) out.readObject();
+
+
+            System.out.println("Object has been de-serialized"+stfObj);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+//Examples.Student@3d82c5f3
