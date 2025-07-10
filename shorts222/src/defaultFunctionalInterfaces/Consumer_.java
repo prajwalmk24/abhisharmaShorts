d6e589428373c517b8f7e3337702d814971ea49f
prajwal mk <mkprajwal65@gmail.com>
2025-07-10 19:16:33 +0530

    serializing with static field

--- shorts222/src/defaultFunctionalInterfaces/Consumer_.java
+++ shorts222/src/defaultFunctionalInterfaces/Consumer_.java
@@ -1,5 +1,6 @@
 package defaultFunctionalInterfaces;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class Consumer_ {
@@ -14,12 +15,23 @@ public class Consumer_ {
         System.out.println("s" + s);
     }
 
+    public Consumer_(String s,Integer i) {
+        System.out.println("s" + s);
+    }
+
     public static void main(String[] args) {
 //        Consumer<String> printUpper = s -> System.out.println(s.toUpperCase());
-
+        System.out.println(new Consumer_());
         Consumer<String> printUpper = Consumer_::new;
 
         System.out.println("printUpper -> " + printUpper);
         printUpper.accept("hello world");
+
+        // two arguements
+
+        BiConsumer<String, Integer> stringStringBiConsumer = (s1, s2)-> {
+            System.out.println(s1+s2);;
+        };
+        stringStringBiConsumer.accept("Prajwal ", 2);
     }
 }
